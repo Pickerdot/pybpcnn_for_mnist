fe9f924a5301e61d058c9690443371036f82977e
Picker <picker.tomato@gmail.com>
2019-07-31 15:43:43 +0900

    up from note

--- function.py
+++ function.py
@@ -3,19 +3,28 @@
 """
 import numpy as np
 
+# シグモイド
 
-def in2col(img, h_filter, w_filter, stride=1, pad=0):
-    H, W = img.shape
-    h_out = (H + 2*pad - h_filter)//stride + 1
-    w_out = (H + 2*pad - w_filter)//stride + 1
-    img = np.pad(img, [(0, 0), (pad, pad), (pad, pad)], 'constant')
 
-    ancol = np.zeros((h_out*w_out, h_filter * w_filter))
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x-1e-7))
 
-    for y_origin in range(h_out):
-        for x_origin in range(w_out):
-            for y in range(h_filter):
-                for x in range(w_filter):
-                    ancol[h_out*y_origin + x_origin, y * h_filter +
-                          x] = img[y_origin * stride + y, x_origin * stride + x]
-    return ancol
+# シグモイド勾配
+
+
+def sigmoid_grad(z):
+    return sigmoid(z) * (1 - sigmoid(z))
+
+
+class SGD:
+    def __init__(self, lr=0.01):
+        self.lr = lr
+
+    def update(self, params, grads):
+        for i in range(len(params)):
+            params[i] -= self.lr * grads[i]
+
+        return params
+
+    def change_lr(self, lr):
+        self.lr = lr
---
+++ layer.py
@@ -0,0 +1,64 @@
+import numpy as np
+import function as fn
+
+
+class Loss:
+    def __init__(self):
+        self.Loss = None
+        self.dout = None
+
+    def forward(self, out, x):
+        self.Loss = 1/2 * np.sum((out - t)**2)
+        self.dout = out - t
+        return self.Loss
+
+    def backward(self):
+        return self.dout
+
+
+class BPneuron:
+    def __init__(self, W, b, lr):
+        # 入力側からの重みと自ニューロンへのバイアスの格納
+        self.W = W
+        self.b = b
+        # W、bに対する勾配の入れ物
+        self.W_grad = None
+        self.b_grad = None
+        # 活性値と出力を格納
+        self.y = None
+        self.z = None
+        # 入力の格納
+        self.x = None
+        # 学習率の格納
+        self.lr = lr
+        self.optimizer = SGD(lr)
+
+    def forward(self, x):
+        # x格納
+        self.x = x
+        # y：活性値
+        self.y = np.dot(x, self.W) + self.b
+        # Z：ニューロンの出力
+        self.z = sigmoid(y)
+        return z
+
+    def backward(self, dz):
+        # 出力部の逆伝搬（シグモイド版）
+        dy = sigmoid_grad(z) * dz
+        self.b_grad = dy
+        self.W_grad = np.dot(self.x.T, dy)
+        dx = np.dot(dy, W.T)
+
+        print("dw:", self.W_grad)
+        print("W:", W)
+
+        # オプティマイザーによりself.W、self.bの値を更新
+        self.W = self.optimizer.update(self.W, self.W_grad)
+        self.b = self.optimizer.update(self.b, self.b_grad)
+
+        print("W:", W)
+
+        return dx self.b
+
+    def change_lr(self, New_lr):
+        self.optimizer.change_lr(New_lr)
